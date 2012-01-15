bf939c82a6f30bd7d62bd2687311bfd7640d9863
Jefferson Girao <contato@jefferson.eti.br>
2012-01-15 19:05:20 -0300

    Adding customized follow redirects stuff to follow 307

--- lib/dwolla/response/parse_json.rb
+++ lib/dwolla/response/parse_json.rb
@@ -4,6 +4,12 @@ require 'multi_json'
 module Dwolla
   module Response
     class ParseJson < Faraday::Response::Middleware
+      def on_complete(env)
+        if respond_to? :parse
+          env[:body] = parse(env[:body]) unless [204,302,304,307].index env[:status]
+        end
+      end
+
       def parse(body)
         case body
         when ''
